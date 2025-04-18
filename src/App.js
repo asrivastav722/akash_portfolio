907d5bdaf5ed75972ca95b5513ff19ff39fe6a2a
unknown <asrivastav722>
2025-04-18 15:48:12 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import { Routes, Route } from 'react-router-dom';
 import Layout from "./Layout/Layout";
 import About from './Pages/Dashboard/Dashboard';
-import Abstract from './Pages/Abstract';
+// import Abstract from './Pages/Abstract';
 import Courseworks from './Pages/Courseworks/Courseworks';
 import Education from './Pages/Education/Education';
 import Skills from './Pages/Skills/Skills';
 import Experience from './Pages/Experience/Experience';
-import Contact from './Pages/Contact';
+import Contact from './Pages/Contact/Contact';
 import Projects from './Pages/Projects/Projects';
 
 function App() {
 
   const HomeRoutes={
-    "/abstract":<Abstract/>,
+    // "/abstract":<Abstract/>,
     "/contact":<Contact/>,
     "/cousework":<Courseworks/>,
     "/education":<Education/>,
     "/projects":<Projects/>,
     "/skills":<Skills/>,
-    "/experience":<Experience/>
+    "/experience":<Experience/>,
+    "/contactme":<Contact/>
   }
+
 
   return (
           <Routes>
